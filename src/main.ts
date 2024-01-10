d066577ad5843200cab50dbb42d8363b9322b33b
bryzikm <bryzikm@gmail.com>
2024-01-10 18:03:58 +0100

    feat: added helmet to the project

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
 import * as express from 'express';
+import helmet from 'helmet';
 
 import { AppModule } from './app.module';
 
@@ -9,6 +10,7 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
 
   app.enableCors();
+  app.use(helmet());
   app.use(express.json({ limit: '10kb' }));
   app.use(express.urlencoded({ limit: '10kb', extended: true }));
 
